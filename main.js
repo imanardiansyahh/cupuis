ef9a5acfc6ab50f027a68f9df33535140c5e78aa
imanardiansyahh <imanardiansyah89@gmail.com>
2020-12-29 17:37:05 +0800

    ewako js

---
+++ main.js
@@ -0,0 +1,44 @@
+// const name = "iman"
+
+// const age = "31"
+
+
+// // concat string 
+// console.log('hyyyy my name is ' + name  + ' and i am ' + age)
+
+// // template iteral 
+// const hello = `hallo guys my name is ${ name} and i am ${age} years old`
+
+// console.log(hello)
+
+
+// const s = 'hello world'
+// const s = 'technology, computers, it, code'
+
+// // console.log(s.length)
+// // console.log(s.toLocaleUpperCase())
+// // console.log(s.toLowerCase())
+// // console.log(s.substring(0, 5).toLocaleUpperCase())
+// console.log(s.split(', '))
+
+
+
+// Arrays
+
+const fruits = ['Apples', 'Oranges', 'Pears' ]
+
+fruits[3]= "grapes"
+
+fruits.push('mangos')
+
+fruits.unshift('strawberry')
+
+// fruits.pop()
+
+console.log(Array.isArray(fruits))
+// console.log(Array.isArray('mangos'))
+
+console.log(fruits.indexOf('Pears'))
+
+
+console.log(fruits)
